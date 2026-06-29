9563e3c5738c40f2156ff2048b15d04c641dac89
Infosoft Corporation <mishra.anay@icloud.com>
2026-06-29 15:40:15 +0200

    Create sysinf.hpp

---
+++ Infosoft Sandboxed Utility / Infosoft Codespaces /sysinf.hpp
@@ -0,0 +1,41 @@
+#include <windows.h>
+#include <raylib.h>
+#include <iostream>
+using namespace std;
+
+/*
+2020 - 2026 Infosoft Corporation
+Infosoft Corp has claimed this .hpp file and more{..}
+*/
+
+#define xmp, xpo, drty;
+#define pxp, yth, th;
+
+namespace cl1__6
+{
+    class c1
+    {
+        public:
+          wc.loader_npe        = LoadFunctionAssets(900, nullptr);
+          wc.loadframerate     = LoadFSM(90, "type");
+          
+    }
+    class c2_rfu  
+    {
+        std::cout << "Reserved for future use";
+    }
+    class c3_rfu {
+        std::cout << "Reserved for future use";
+    }
+    class c4_rfu{
+          std::cout << "Reserved for future use";
+    }
+    class c5_rfu
+    {
+        std::cout << "Reserved for future use";
+    }
+    class c6_rfu
+    {
+        std::cout << "Reserved for future use";
+    }
+}
